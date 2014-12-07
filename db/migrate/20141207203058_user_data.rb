b223e083a5f1d8d334eb02786546c6a0eb119a3c
Miguel Palhas <mpalhas@gmail.com>
2014-12-07 20:39:06 +0000

    migration

--- db/migrate/20141207203058_user_data.rb
+++ db/migrate/20141207203058_user_data.rb
@@ -1,6 +1,6 @@
 class UserData < ActiveRecord::Migration
   def change
-    add_column :users, :first_name
-    add_column :users, :last_name
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
   end
 end
