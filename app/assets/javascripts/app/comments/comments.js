95886f582ea61fb247e594de89c12d9ff06f45b9
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2014-12-07 20:32:55 +0000

    Adds frontend comments module and controller boilerplate

---
+++ app/assets/javascripts/app/comments/comments.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('two_cents.comments')
+    .controller('CommentsCtrl', CommentsCtrl);
+
+  function CommentsCtrl() {
+
+  }
+})();
---
+++ app/assets/javascripts/app/comments/comments.module.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('two_cents.comments', [
+      'two_cents.core'
+    ]);
+})();
