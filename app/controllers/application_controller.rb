bbc0f64eb54aabd5cb79844a80905819348385fa
Miguel Palhas <mpalhas@gmail.com>
2014-12-07 20:36:21 +0000

    Adds devise and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
+    redirect_to new_user_session_path unless current_user
   end
 end
---
+++ db/migrate/20141207203058_user_data.rb
@@ -0,0 +1,6 @@
+class UserData < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name
+    add_column :users, :last_name
+  end
+end
