c3f29380d597b3b039f213fef241a321f3a56a89
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2014-12-07 20:34:38 +0000

    Updates frontend app imported modules

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,8 +1,15 @@
 (function() {
   'use strict';
 
-  angular.module('two_cents', [
-    'ngMaterial',
-    'templates'
-  ]);
+  angular
+    .module('two_cents', [
+      'ngMaterial',
+      'templates',
+      'ui-router',
+      'angular-data',
+
+      'two_cents.core',
+      'two_cents.discussions',
+      'two_cents.comments'
+    ]);
 })();
