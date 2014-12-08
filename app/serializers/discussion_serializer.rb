75c2bec7796af3965c794b44af1551403bb9e6e9
Miguel Palhas <mpalhas@gmail.com>
2014-12-08 04:17:22 +0000

    Adds comment_count to discussion serializer

--- app/serializers/discussion_serializer.rb
+++ app/serializers/discussion_serializer.rb
@@ -1,5 +1,5 @@
 class DiscussionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :subtitle, :body, :open, :authors
+  attributes :id, :title, :subtitle, :body, :open, :authors, :comment_count
 
   has_one :author, embed: :ids
   has_many :comments, embed: :objects
@@ -9,5 +9,9 @@ class DiscussionSerializer < ActiveModel::Serializer
       User.find([object.author_id] + object.comments.map(&:author_id).uniq).sort_by(&:id),
       each_serializer: UserSerializer
     ).serializable_object
+  end
+
+  def comment_count
+    object.comments.count
   end
 end
