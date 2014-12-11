2e9086cc26de376338a28d1b8421e11129bdc14a
Bruno Azevedo <azevedo.252@gmail.com>
2014-12-11 01:28:12 +0000

    Refactored theme; Created style for buttons

---
+++ app/assets/javascripts/app/components/button/button.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('council.components')
+    .directive('councilFaButton', councilFaButton)
+    .directive('councilRaisedButton', councilRaisedButton)
+    .directive('councilFlatButton', councilFlatButton);
+
+  function councilFaButton() {
+    return {
+      restrict: 'E',
+      scope: {},
+      link: councilFaButtonPostLink,
+      templateUrl: 'components/button/templates/floating_action.html'
+    }
+  }
+
+  function councilFaButtonPostLink(scope, element, attrs) {
+    if (!attrs.type) { attrs.type = 'primary' };
+    if (!attrs.icon) { attrs.icon = 'add' };
+    scope.type = attrs.type;
+    scope.icon = attrs.icon;
+    scope.ariaLabel = attrs.ariaLabel;
+  }
+
+  function councilRaisedButton() {
+    return {
+      restrict: 'E',
+      scope: {},
+      transclude: true,
+      link: councilRaisedButtonPostLink,
+      templateUrl: 'components/button/templates/raised.html'
+    }
+  }
+
+  function councilRaisedButtonPostLink(scope, element, attrs) {
+    if (!attrs.type) { attrs.type = 'secondary' };
+    scope.type = attrs.type;
+  }
+
+  function councilFlatButton() {
+    return {
+      restrict: 'E',
+      scope: {},
+      transclude: true,
+      templateUrl: 'components/button/templates/flat.html'
+    }
+  }
+})();
