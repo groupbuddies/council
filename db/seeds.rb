78a0dacd45869b1cc11b7c34c5045061dbac2d36
Miguel Palhas <mpalhas@gmail.com>
2014-12-11 13:37:20 +0000

    Fixes README links and bin/setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create! [
-  { first_name: 'Miguel',  last_name: 'Palhas',   email: 'mpalhas@groupbuddies.com', password: 'mpalhaspassword' },
-  { first_name: 'João',    last_name: 'Ferreira', email: 'joao@groupbuddies.com',    password: 'joaopassword' },
-  { first_name: 'Roberto', last_name: 'Machado',  email: 'roberto@groupbuddies.com', password: 'robertopassword' },
-  { first_name: 'Luís',    last_name: 'Ferreira', email: 'zamith@groupbuddies.com',  password: 'zamithpassword' },
-  { first_name: 'Bruno',   last_name: 'Azevedo',  email: 'bruno@groupbuddies.com',   password: 'brunopassword' },
-  { first_name: 'Ronaldo', last_name: 'Sousa',    email: 'ronaldo@groupbuddies.com', password: 'ronaldopassword' },
-  { first_name: 'João',    last_name: 'Justo',    email: 'jjusto@groupbuddies.com',  password: 'jjustopassword' },
-  { first_name: 'Gabriel', last_name: 'Poça',     email: 'gabriel@groupbuddies.com', password: 'gabrielpassword' }
+
+users = [
+  { first_name: 'Miguel',  last_name: 'Palhas',   email: 'mpalhas@groupbuddies.com' },
+  { first_name: 'João',    last_name: 'Ferreira', email: 'joao@groupbuddies.com' },
+  { first_name: 'Roberto', last_name: 'Machado',  email: 'roberto@groupbuddies.com' },
+  { first_name: 'Luís',    last_name: 'Ferreira', email: 'zamith@groupbuddies.com' },
+  { first_name: 'Bruno',   last_name: 'Azevedo',  email: 'bruno@groupbuddies.com' },
+  { first_name: 'Ronaldo', last_name: 'Sousa',    email: 'ronaldo@groupbuddies.com' },
+  { first_name: 'João',    last_name: 'Justo',    email: 'jjusto@groupbuddies.com' },
+  { first_name: 'Gabriel', last_name: 'Poça',     email: 'gabriel@groupbuddies.com' }
 ]
+
+users.each do |user_data|
+  User.where(user_data).first_or_create(password: user_data[:email].split('@').first + 'password')
+end
