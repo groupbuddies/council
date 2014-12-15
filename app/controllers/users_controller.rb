26cb6553b5e3b8d4b1e61511facbcd9e7bc6fa15
Miguel Palhas <mpalhas@gmail.com>
2014-12-15 11:53:36 +0000

    Rubocop green

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,30 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.json { render json: @user }
-      end
+      respond_with_success
     else
-      respond_to do |format|
-        format.html do
-          flash[:error] = @user.errors.full_messages.to_sentence
-          render :edit
-        end
-        format.json { render json: { error: @user.errors.full_messages }.to_json, status: 422 }
-      end
+      respond_with_error
     end
   end
 
   private
+
+  def respond_with_success
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { render json: @user }
+    end
+  end
+
+  def respond_with_error
+    respond_to do |format|
+      format.html do
+        flash[:error] = @user.errors.full_messages.to_sentence
+        render :edit
+      end
+      format.json { render json: { error: @user.errors.full_messages }.to_json, status: 422 }
+    end
+  end
 
   def user_params
     params.require(:user).permit(:password, :password_confirmation)
