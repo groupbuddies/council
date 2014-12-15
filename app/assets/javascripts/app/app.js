d7cbbc1206a9e5325ef92669e7103615b680e407
Miguel Palhas <mpalhas@gmail.com>
2014-12-15 18:38:55 +0000

    Adds loading spinner

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -15,7 +15,12 @@
       'council.members',
       'btford.markdown'
     ])
-    .config(csrf);
+    .config(csrf)
+    .run(function ($rootScope) {
+      $rootScope.$on('$stateChangeStart', function(event, toState) {
+        console.log('[DEBUG] Route change: ', toState);
+      })
+    });
 
     function csrf($httpProvider) {
       $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
---
+++ app/assets/javascripts/app/components/waitFor/waitFor.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('council.components')
+    .directive('waitFor', waitFor);
+
+  function waitFor($compile) {
+    return {
+      restrict: 'A',
+      transclude: true,
+      scope: {
+        pageReady: '&waitFor'
+      },
+      templateUrl: 'components/waitFor/templates/wait_for.html',
+      bindToController: true
+    }
+  }
+})()
