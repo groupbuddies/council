92792b483d7a2fed08cc8a08082dd600e3d46a68
João Justo <jpjustonunes@hotmail.com>
2014-12-15 19:09:44 +0000

    Discussion page style

--- app/serializers/discussion_serializer.rb
+++ app/serializers/discussion_serializer.rb
@@ -1,5 +1,5 @@
 class DiscussionSerializer < EditableSerializer
-  attributes :id, :title, :subtitle, :body, :open, :comments_count
+  attributes :id, :title, :subtitle, :body, :open, :comments_count, :created_at
 
   has_one :author, embed: :ids
   has_many :comments, embed: :objects
