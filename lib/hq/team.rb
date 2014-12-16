5af243275333bc0ef7cb5cd319620c94ba48f0eb
Miguel Palhas <mpalhas@gmail.com>
2014-12-16 01:06:38 +0000

    Adds private_class_method

--- lib/hq/team.rb
+++ lib/hq/team.rb
@@ -5,10 +5,11 @@ class Hq::Team
   base_uri 'hq.groupbuddies.com'
 
   def self.all
-    get_json('/')
+    get_json('/members')
   end
 
   def self.get_json(path)
     get(path).to_json
   end
+  private_class_method :get_json
 end
