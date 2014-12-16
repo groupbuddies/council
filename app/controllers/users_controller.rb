268e4c696894eb0e7adc129f576d9500ce3b2fb1
Miguel Palhas <mpalhas@gmail.com>
2014-12-16 00:59:05 +0000

    Removes devise password views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,23 @@
 class UsersController < ApplicationController
-  authorize_resource
 
   def edit
     @user = User.find(params[:id])
+    authorize! :manage, @user
   end
 
   def update
     @user = User.find(params[:id])
+    authorize! :manage, @user
 
     if @user.update(user_params)
-      respond_with_success
+      redirect_to root_path
     else
-      respond_with_error
+      flash[:error] = @user.errors.full_messages.to_sentence
+      render :edit
     end
   end
 
   private
-
-  def respond_with_success
-    format.html { redirect_to root_path }
-  end
-
-  def respond_with_error
-    flash[:error] = @user.errors.full_messages.to_sentence
-    render :edit
-  end
 
   def user_params
     params.require(:user).permit(:password, :password_confirmation)
