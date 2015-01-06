dd250b710c0924215ceab6773a3c6ed36180f228
João <jpjustonunes@hotmail.com>
2015-01-06 16:56:24 +0000

    Refatored log in page to use default angular material components

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -16,6 +16,7 @@
       'btford.markdown'
     ])
     .config(csrf)
+    .config(theme)
     .run(function ($rootScope) {
       $rootScope.$on('$stateChangeStart', function(event, toState) {
         console.log('[DEBUG] Route change: ', toState);
@@ -24,5 +25,12 @@
 
     function csrf($httpProvider) {
       $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+    }
+
+    function theme($mdThemingProvider) {
+      $mdThemingProvider.theme('council')
+        .primaryColor('blue-grey')
+        .accentColor('amber')
+        .warnColor('teal');
     }
 })();
