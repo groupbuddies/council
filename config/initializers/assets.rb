5fa9529037ae4e8e58608bb64d163ca92eb68e9f
Gabriel Poça <gabrielpoca@gmail.com>
2015-01-12 14:33:19 +0000

    QUICKFIX: trying to fix heroku not compiling images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( log_in.css )
-Rails.application.config.assets.precompile += %w( emojis.png emojis@2x.png )
+Rails.application.config.assets.precompile += %w( *.png )
