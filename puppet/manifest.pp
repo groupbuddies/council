1e9b39a95d6051a9e2a3d840795f2fba84dd5263
Miguel Palhas <mpalhas@gmail.com>
2015-02-03 09:41:21 +0000

    Adds bower as a puppet dependency

--- puppet/manifest.pp
+++ puppet/manifest.pp
@@ -2,6 +2,11 @@ $data = hiera('gb-council')
 
 gb::ruby { 'ruby-2.2.0': }
 
+package { 'bower':
+  ensure => present,
+  provider => 'npm',
+}
+
 gb::postgresql { 'gb-council':
   password => $data[db_password]
 }
