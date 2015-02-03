15eed183a3b685e41a11e31839036f143cff0425
Miguel Palhas <mpalhas@gmail.com>
2015-02-03 11:02:08 +0000

    Links bower_components directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :pty, true
 set :rvm_ruby_version, '2.2.0'
 
 set :linked_files, %w{.env}
-set :linked_dirs, %w{log public/system public/uploads}
+set :linked_dirs, %w{log public/system public/uploads vendor/assets/bower_components}
 
 set :bundle_without, %w(development test deploy).join(' ')
 set :keep_releases, 3
