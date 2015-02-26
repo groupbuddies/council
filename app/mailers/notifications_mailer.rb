5477024fa9708629596a612688aa86fb3efad5ca
Miguel Palhas <mpalhas@gmail.com>
2015-02-26 12:13:20 +0000

    Fixes email subject and body

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -7,7 +7,9 @@ class NotificationsMailer < ActionMailer::Base
     @discussion = @notification.discussion
     @url = root_url(anchor: anchor_for_notification(@notification))
 
-    mail to: @notification.email, subject: '[Council] New discussion', template_name: @notification.kind
+    mail to: @notification.email,
+         subject: subject_for_notification(@notification),
+         template_name: @notification.kind
   end
 
   private
@@ -16,6 +18,13 @@ class NotificationsMailer < ActionMailer::Base
     case notification.kind
     when 'new_discussion' then "/discussions/#{notification.discussion.id}"
     when 'new_comment'    then "/discussions/#{notification.discussion.id}/comments/#{notification.discussion.comments.last.id}"
+    end
+  end
+
+  def subject_for_notification(notification)
+    case notification.kind
+    when 'new_discussion' then '[Council] New discussion'
+    when 'new_comment'    then '[Council] New comment'
     end
   end
 end
