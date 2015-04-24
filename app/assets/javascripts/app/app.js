53d4d0da8ec1affa90568d868bf376731149aaeb
Gabriel Poça <gabrielpoca@gmail.com>
2015-04-24 01:12:30 +0100

    Fix scroll on mobile devices
    Changing the header to fixed prevents it being scrolled with the main
    body.
    Disabling the mouchemove form everything but the content prevents the
    page from overscrolling on touch events.
    md-cards has overscroll enabled that causes a strange scroll on
    the discussion details.

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -17,18 +17,26 @@
       'dbaq.emoji'
     ])
     .config(csrf)
-    .config(theme);
+    .config(theme)
+    .run(preventOverscroll);
 
-    function csrf($httpProvider) {
-      $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-    }
+  function csrf($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  }
 
-    function theme($mdThemingProvider) {
-      $mdThemingProvider.theme('council')
-        .primaryColor('blue-grey', {
-          'hue-1': 'A700'
-        })
-        .accentColor('amber')
-        .warnColor('teal');
-    }
+  function theme($mdThemingProvider) {
+    $mdThemingProvider.theme('council')
+      .primaryColor('blue-grey', {
+        'hue-1': 'A700'
+      })
+      .accentColor('amber')
+      .warnColor('teal');
+  }
+
+  function preventOverscroll($document) {
+    $document.find('body').on('touchmove', function(e) {
+      if (!$('.TopBarLayout-content').has($(e.target)).length)
+        e.preventDefault();
+    });
+  }
 })();
